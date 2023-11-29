4a344315f0a8b81e285460429522240510fd22a7
Henrique de Carvalho <decarv.henrique@gmail.com>
2023-11-28 21:19:11 -0300

    Update repo/searcher.py

--- labrador/repo/searcher.py
+++ labrador/repo/searcher.py
@@ -1,3 +1,4 @@
+import re
 import asyncio
 import time
 
@@ -19,29 +20,43 @@ class RepositorySearcher(Searcher):
             raise ValueError("Missing required argument: database.")
 
     def _retrieve(self, query: str, top_k):
-        base_url: str = config.THESES_QUERY_URL.format(query.replace(" ", "%20"))
+        query = query.lower().strip().replace(" ", "%20")
+        theses_query_url: str = (
+            "https://www.teses.usp.br/index.php?option=com_jumi&fileid=19&Itemid=87&lang=pt&g=1&b1={}&c1=t&o1=OR&b2={}&c2=r&o2=OR&b3={}&c3=p&o3=OR&pagina="
+        )
+        base_url: str = theses_query_url.format(query, query, query)
         page = 1
         documents_urls: list[str] = []
         results: list[str] = []
-        while len(results) < top_k:
+        inserted: set[str] = set()
+        while len(documents_urls) < top_k:
             url: str = base_url + str(page)
             response: requests.Response = requests.get(url)
             if response is not None:
                 soup = bs4.BeautifulSoup(response.content, 'html.parser')
+                if self.last_page(soup):
+                    break
                 divs = soup.find_all(
                     "div",
                     class_=["dadosLinha dadosCor1", "dadosLinha dadosCor2"]
                 )
-                documents_urls += [div.a['href'] for div in divs]
-            paths: list[str] = [Processor.extract_path_suffix(url) for url in documents_urls]
-            results.extend(self.db.select(
-                """SELECT d.id as doc_id, d.title_pt as title, d.abstract_pt as abstract, d.keywords_pt as keywords, 
-                          d.author, d.url
-                     FROM documents as d 
-                    WHERE d.url_path_suffix = ANY(%s);""", var_args=(paths,)))
+                for div in divs:
+                    url = div.a['href']
+                    if url not in inserted:
+                        inserted.add(url)
+                        documents_urls.append(url)
             page += 1
+        paths: list[str] = [Processor.extract_path_suffix(url) for url in documents_urls]
+        results.extend(self.db.select(
+            """SELECT d.id as doc_id, d.title_pt as title, d.abstract_pt as abstract, d.keywords_pt as keywords, 
+                      d.author, d.url
+                 FROM documents as d 
+                WHERE d.url_path_suffix = ANY(%s);""", var_args=(paths,)))
         return results
 
+    @staticmethod
+    def last_page(soup):
+        return re.search(r"\b0\b", soup.find("div", class_="dadosLinha").text) is not None
 
     async def _retrieve_async(self, query: str, top_k) -> list[dict]:
         base_url: str = config.THESES_QUERY_URL.format(query.replace(" ", "%20"))
@@ -93,7 +108,7 @@ class RepositorySearcher(Searcher):
 if __name__ == '__main__':
     from labrador.util.database import Database
 
-    query = "aprendizagem de máquina com iot"
+    query = "sonologia"
     start = time.time()
     searcher = RepositorySearcher(database=Database())
     results = searcher.search(query, top_k=10)
